e73c2ecc59320ec96fdd2b31e99ae8d502b76641
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-10-09 13:31:09 +1100

    feat: Add table definitions

--- tournament.sql
+++ tournament.sql
@@ -1,1 +1,17 @@
 -- Table definitions for the tournament project.
+
+
+CREATE TABLE players (
+    id      serial PRIMARY KEY,
+    name    varchar(40) NOT NULL
+)
+
+
+CREATE TABLE matches (
+    id      serial PRIMARY KEY,
+    player1 integer REFERENCES players (id),
+    player2 integer REFERENCES players (id),
+    winner  integer REFERENCES players (id),
+    CHECK (player1 != player),
+    CHECK (winner = player1 or winner = player2)
+)
