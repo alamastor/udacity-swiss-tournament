1822040e2d55711245d9baac0cd07d76e22259f0
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-10-09 13:45:32 +1100

    fix: Typo in constraint

--- tournament.sql
+++ tournament.sql
@@ -4,7 +4,7 @@
 CREATE TABLE players (
     id      serial PRIMARY KEY,
     name    varchar(40) NOT NULL
-)
+);
 
 
 CREATE TABLE matches (
@@ -12,6 +12,6 @@ CREATE TABLE matches (
     player1 integer REFERENCES players (id),
     player2 integer REFERENCES players (id),
     winner  integer REFERENCES players (id),
-    CHECK (player1 != player),
+    CHECK (player1 != player2),
     CHECK (winner = player1 or winner = player2)
-)
+);
